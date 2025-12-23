9a2d1e3f426e47811c166cdd22cc3c761ae0c624
bala-amypo <bala@amypo.in>
2025-12-23 08:08:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/AllocationRuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.demo.resourceallocation.repository;
-
-import com.demo.resourceallocation.entity.AllocationRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllocationRuleRepository
-        extends JpaRepository<AllocationRule, Long> {
-}
