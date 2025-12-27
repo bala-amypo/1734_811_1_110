2f5aa92638ef720fc147caf8095da70e6920b5cf
bala-amypo <bala@amypo.in>
2025-12-27 04:17:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationRule.java
+++ src/main/java/com/example/demo/entity/AllocationRule.java
@@ -1,37 +1,14 @@
-package com.example.demo.entity;
-
-import jakarta.persistence.*;
-
-@Entity
-public class AllocationRule {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String ruleName;
-
-    private int priorityWeight;
-
-    public AllocationRule() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getRuleName() {
-        return ruleName;
-    }
-
-    public void setRuleName(String ruleName) {
-        this.ruleName = ruleName;
-    }
-
-    public int getPriorityWeight() {
-        return priorityWeight;
-    }
-
-    public void setPriorityWeight(int priorityWeight) {
-        this.priorityWeight = priorityWeight;
-    }
-}
+package com.example.demo.entity; 
+import jakarta.persistence.*; 
+import lombok.Data; 
+@Entity 
+@Data 
+public class AllocationRule { 
+@Id 
+@GeneratedValue(strategy = GenerationType.IDENTITY) 
+private Long id; 
+@Column(unique = true) 
+private String ruleName; 
+private String ruleType; 
+private Integer priorityWeight; 
+} 
