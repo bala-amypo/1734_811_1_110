2e1ab24c1b7bda224d61acffc02e74b59f2b0e9e
bala-amypo <bala@amypo.in>
2025-12-27 04:41:23 +0000

    User commit

--- src/main/java/com/example/demo/service/ResourceService.java
+++ src/main/java/com/example/demo/service/ResourceService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 import com.example.demo.entity.Resource; 
 import java.util.List; 
 public interface ResourceService { 
-// Create a resource (throws exception if exists/invalid) 
 Resource createResource(Resource resource); 
 List<Resource> getAllResources(); 
 Resource getResourceByName(String resourceName); 
